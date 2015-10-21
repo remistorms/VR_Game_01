7b826653498efee1d874e05d64b2da92fadd2d51
remistorms <ultimate.spidey.18@gmail.com>
2015-10-20 23:25:16 -0500

    Added Lobby Mesh Added Stones of Hope Models Added Basic funcionality

---
+++ Assets/Scripts/RotateScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateScript : MonoBehaviour {
+
+	GameObject thisObject;
+	public float rotateSpeed = 5.0f;
+	public Vector3 rotation_vector = new Vector3(0,1,0);
+	// Use this for initialization
+	void Start () 
+	{
+		thisObject = this.gameObject;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		thisObject.transform.RotateAround (rotation_vector, rotateSpeed * Time.deltaTime);
+	}
+}
